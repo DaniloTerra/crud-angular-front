f15d994a0bb9f2f541ab04736c7795dde0821569
DaniloTerra <danilo_terra@live.com>
2015-11-29 16:25:06 -0200

    Ação de incluir cliente por meio do form.html

--- js/controller/clienteFormController.js
+++ js/controller/clienteFormController.js
@@ -1,4 +1,30 @@
-$app.controller('clienteFormController', ['$scope', '$routeParams','$route', function($scope, $routeParams, $route) {
-  $scope.titulo = 'Form';
-  $scope.action = $route.current.action;
+$app.controller('clienteFormController',
+['$scope', '$routeParams','$route', '$http',
+function($scope, $routeParams, $route, $http) {
+  $scope.isAlterando = $route.current.action == 'alterar';
+  $scope.isIncluindo = $route.current.action == 'incluir';
+  $scope.cliente     = null;
+  $scope.acao        = "ação";
+
+  $scope.loadCliente = function() {
+    if ($routeParams.id != null) {
+      $scope.get(SERVER_URL+'/cliente/'+$routeParams.id).success(function(data) {
+        $scope.cliente = data;
+      });
+    } else {
+      $scope.cliente = {};
+    }
+  };
+
+  $scope.salvar = function() {
+    if ($scope.isAlterando) {
+      $http.put(SERVER_URL+'/cliente/'+$routeParams.id, $scope.cliente).success(function(data) {
+        alert('Alterado com sucesso');
+      });
+    } else if ($scope.isIncluindo) {
+      $http.post(SERVER_URL+'/cliente', $scope.cliente).success(function(data) {
+        alert('Incluído com sucesso');
+      });
+    }
+  }
 }]);
